e6027ff84193b0b3b0ccc9e2625d2bd60d8f62b7
RyujiOzawa <uni202406_05@telework.to>
2024-11-14 12:36:25 +0900

    マップの多様化

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -8,12 +8,19 @@ using UnityEngine;
 // ・大きさを決める
 public class MapGenerator : MonoBehaviour
 {
-    [SerializeField] TileObj tileObjPrefab;
+    [SerializeField] TileObj grassPrefab;
+    [SerializeField] TileObj forestPrefab;
+    [SerializeField] TileObj waterPrefab;
     [SerializeField] Transform tileParent;
+
+    // Prefabの多様化:バリアント
+    // 割合に応じたPrefabの生成
 
     const int WiDTH = 15;
     const int HEIGHT = 9;
-
+    const int WATER_RATE = 10;
+    const int FOREST_RATE = 30;
+    
     private void Start()
     {
         Generate();
@@ -26,8 +33,20 @@ public class MapGenerator : MonoBehaviour
         {
             for (int y = 0; y < HEIGHT; y++)
             {
-                Vector2 pos = new Vector2(x, y);
-                Instantiate(tileObjPrefab, pos, Quaternion.identity, tileParent);
+                Vector2 pos = new Vector2(x, y) + offset;
+                int rate = Random.Range(0, 100); // 0-99迄の数字がランダムで1つ出る
+                if (rate < 10)
+                {
+                    Instantiate(waterPrefab, pos, Quaternion.identity, tileParent);
+                }
+                else if (rate < 30)
+                {
+                    Instantiate(forestPrefab, pos, Quaternion.identity, tileParent);
+                }
+                else
+                {
+                    Instantiate(grassPrefab, pos, Quaternion.identity, tileParent);
+                }
             }
         }
     }
