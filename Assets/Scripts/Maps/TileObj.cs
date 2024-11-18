7b42866e8368a2e3d2a6c7cf3f31a407f5c2dfaa
RyujiOzawa <uni202406_05@telework.to>
2024-11-18 14:16:23 +0900

    移動コストの実装

--- Assets/Scripts/Maps/TileObj.cs
+++ Assets/Scripts/Maps/TileObj.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class TileObj : MonoBehaviour
 {
     public Vector2Int positionInt;
+    [SerializeField ]int cost;// 移動コスト
 
     [SerializeField] GameObject movablePanel;
+
+    public int Cost { get => cost; }
 
     public void ShowMovablePanel(bool isActive)
     {
         movablePanel.SetActive(isActive);
     }
+
+    public void SetCost(int cost)
+    {
+        this.cost = cost;
+    }
 }
+
+// TODO:地形に依存した移動範囲の表示
+// ・地形の移動コストを其々のタイルに設定する
+// ・移動コストに基づいて、移動範囲を計算する
